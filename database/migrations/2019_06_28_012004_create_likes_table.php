d97e56a74deeb0a3d3abd45ade6d94831646d51c
luisgpm <luisgpm2@gmail.com>
2019-06-27 20:53:02 -0500

    migraciones

---
+++ database/migrations/2019_06_28_012004_create_likes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLikesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('likes', function (Blueprint $table) {
+            $table->integer('like_id');
+            $table->integer('publicacion_id')
+            $table->integer('usuario_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('likes');
+    }
+
+    public function publicacion()
+    {
+        return $this->belongsto('publicacion');
+    }
+}
